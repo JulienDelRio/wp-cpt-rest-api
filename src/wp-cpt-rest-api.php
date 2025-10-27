d89a751d48caee83e1757a6aea35e44c6b984302
Julien Del Rio <julien.delrio@gmail.com>
2025-10-27 11:21:35 +0100

    Change license from Apache 2.0 to GPLv2 or later (#5)
    Updated license to comply with WordPress.org plugin directory requirements.
    
    Changes:
    - Replaced LICENSE file with official GPLv2 license text
    - Updated plugin header in wp-cpt-rest-api.php
    - Updated readme.txt license field
    - Changed License URI to GPLv2 official link
    
    This change is necessary for submitting the plugin to the WordPress.org
    plugin directory, which requires GPL-compatible licenses.

--- src/wp-cpt-rest-api.php
+++ src/wp-cpt-rest-api.php
@@ -6,8 +6,8 @@
  * Version: 1.0.0
  * Author: Julien DELRIO
  * Author URI: https://juliendelrio.fr
- * License: Apache 2.0
- * License URI: http://www.apache.org/licenses/LICENSE-2.0
+ * License: GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Requires at least: 6.0
  * Tested up to: 6.6
  * Requires PHP: 7.4
